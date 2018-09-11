52a455c2d0048e3c20e0a1054ab97fbef326452f
today-jin <293551273@qq.com>
2018-09-11 10:03:11 +0800

    Create 1-two-sum-2.cpp
    这次战胜了96%

---
+++ 1-two-sum-2.cpp
@@ -0,0 +1,30 @@
+/*给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i) {
+            m[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size()-1; ++i) {
+            int t = target - nums[i];
+            if (m.count(t) && m[t] != i) {
+                result.push_back(i);
+                result.push_back(m[t]);
+                break;
+            }
+        }
+        return result;
+    }
+};
